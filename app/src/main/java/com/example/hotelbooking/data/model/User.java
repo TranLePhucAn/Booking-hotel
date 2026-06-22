18e0bf00f5174107c8681976cef32b83b1122921
itlord23 <nguyenthihonghanh5098@gmail.com>
2026-06-22 14:05:00 +0700

    [Update] Xử lý thanh toán bằng credit card và qr

--- app/src/main/java/com/example/hotelbooking/data/model/User.java
+++ app/src/main/java/com/example/hotelbooking/data/model/User.java
@@ -23,5 +23,6 @@ public class User {
     public void setFullName(String fullName) { this.fullName = fullName; }
 
     public String getEmail() { return email; }
+
     public void setEmail(String email) { this.email = email; }
 }
