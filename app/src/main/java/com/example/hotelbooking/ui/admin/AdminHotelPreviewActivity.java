30ac044fa83580b092aed1b9a569efa7cd596950
TranLePhucAn <23130004@st.hcmuaf.edu.vn>
2026-06-28 23:55:20 +0700

    Xóa các activity rỗng không sử dụng

--- app/src/main/java/com/example/hotelbooking/ui/admin/AdminHotelPreviewActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.hotelbooking.ui.admin;
-
-public class AdminHotelPreviewActivity {
-}
--- app/src/main/java/com/example/hotelbooking/ui/welcome/WelcomeActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.hotelbooking.ui.welcome;
-
-public class WelcomeActivity {
-}
